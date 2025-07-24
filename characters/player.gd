0353d31ea47daa64251bf6a2e253ed5c8937a05e
Luis Zhang <luis.zhang@Live.com.au>
2025-07-24 12:17:53 +1000

    Added walk animations to player script

--- characters/player.gd
+++ characters/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D  
 
-@export var speed : float = 300
+@export var speed : float = 100
 
 func _physics_process(delta):  
 	var input_dir = Input.get_vector("left", "right", "up", "down")  
@@ -8,3 +8,16 @@ func _physics_process(delta):
 	# Apply movement  
 	velocity = input_dir.normalized() * speed  
 	move_and_slide()  # Handles collisions automatically  
+	
+	player_animate(input_dir)
+	
+func player_animate(move_input):
+	# Walking and setting idle direction
+	if move_input.y < 0:
+		$AnimationPlayer.play("walk_up")
+	elif move_input.y > 0:
+		$AnimationPlayer.play("walk_down")
+	elif move_input.x < 0:
+		$AnimationPlayer.play("walk_left")
+	elif move_input.x > 0:
+		$AnimationPlayer.play("walk_right")
